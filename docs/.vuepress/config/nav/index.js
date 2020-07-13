e6ddeb6e444681585a68e2737aa7cfcdace8a08c
jiyongchao <jiyongchao.19961>
2020-07-13 17:45:22 +0800

    init

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -4,12 +4,8 @@ module.exports = [
   { text: 'Contact', 
     icon: 'reco-message',
     items: [
-      { text: 'NPM', link: 'https://www.npmjs.com/~reco_luan', icon: 'reco-npm' },
-      { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' },
-      { text: '简书', link: 'https://www.jianshu.com/u/cd674a19515e', icon: 'reco-jianshu' },
-      { text: 'CSDN', link: 'https://blog.csdn.net/recoluan', icon: 'reco-csdn' },
-      { text: '博客圆', link: 'https://www.cnblogs.com/luanhewei/', icon: 'reco-bokeyuan' },
-      { text: 'WeChat', link: 'https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A', icon: 'reco-wechat' },
+      { text: 'GitHub', link: 'https://github.com/jiyongchao-qf', icon: 'reco-github' },
+      { text: '简书', link: 'https://www.jianshu.com/u/8f5d90376485', icon: 'reco-jianshu' },
     ]
   }
 ]
--- docs/views/personalInfo/README.md
+++ docs/views/personalInfo/README.md
@@ -2,7 +2,7 @@
 title: 个人信息
 tags:
  - 个人信息
-date: 2018-09-10
+date: 2020-07-13
 isComment: false
 sidebar: false
 ---
@@ -11,19 +11,11 @@ sidebar: false
 
 ***
  
-`Vue全家桶` `微信JS-SDK` `微信小程序` `ES6` `Sass` `Less` `Stylus` `NodeJS` `vuepress` `fetch` `gulp` `webpack`
-
+`Vue全家桶` `Spring Cloud` 
 #### 我的业余生活
 
 ***
 
-1. [vuepress-theme-reco](https://www.npmjs.com/package/vuepress-theme-reco)<br>
-   一款简介的vuepress博客主题
-2. [reco-fetch](https://www.npmjs.com/package/reco-fetch)<br>
-   针对fetch的应用型封装
-
 个人博客：[**吉永超**](https://jiyongchao-qf.github.io/) 
 
 ***
-
-### 如果有话对我讲，那就请[联系我](https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A)
