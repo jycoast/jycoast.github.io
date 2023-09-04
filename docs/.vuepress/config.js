672534b8a35d525a7ad07cf94614a5fce56af938
jiyongchao <jycoder@163.com>
2023-09-04 11:34:32 +0800

    增加关于我页面

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,76 +20,120 @@ export default defineUserConfig({
         logo: 'mlogo.svg',
         repo: 'https://github.com/jycoast',
         navbar: [
-            { text: 'Java 面试', link: '/interview/' },
-            {
-              text: 'Java 基础', 
+          {
+              text: 'Java 面试',
+              link: '/interview/'
+          },
+          {
+              text: 'Java 基础',
               collapsible: true,
-              children: [
-                { text: '函数式编程', link: '/java_basics/functional_programming/' },
-                { text: 'Java核心技术原理', link: '/java_basics/core_technology/' },
-                { text: 'Java知识体系合辑', link: '/java_basics/knowledge_system/' }
-              ]
-            },
-            {
-              text: '数据结构与算法', children: [
-                { text: '数据结构', link: '/leetcode/data_structure.md' },
-                { text: '算法实战', link: '/leetcode/algorithm.md' }
-              ]
-            },
-            {
+              children: [{
+                  text: '函数式编程',
+                  link: '/java_basics/functional_programming/'
+              },
+              {
+                  text: 'Java核心技术原理',
+                  link: '/java_basics/core_technology/'
+              },
+              {
+                  text: 'Java知识体系合辑',
+                  link: '/java_basics/knowledge_system/'
+              }]
+          },
+          {
+              text: '数据结构与算法',
+              children: [{
+                  text: '数据结构',
+                  link: '/leetcode/data_structure.md'
+              },
+              {
+                  text: '算法实战',
+                  link: '/leetcode/algorithm.md'
+              }]
+          },
+          {
               text: '数据库',
               collapsible: true,
-              children: [
-                {
+              children: [{
                   text: '关系型数据库',
-                  children: [
-                    { text: '深入浅出MySQL', link: '/database/mysql/' },
+                  children: [{
+                      text: '深入浅出MySQL',
+                      link: '/database/mysql/'
+                  },
                   ]
-                },
-                {
+              },
+              {
                   text: 'NoSQL',
-                  children: [
-                    { text: '深入浅出Redis', link: '/database/redis/' },
+                  children: [{
+                      text: '深入浅出Redis',
+                      link: '/database/redis/'
+                  },
                   ]
-                }
-              ]
-            },
-            {
-              text: '常用框架', 
-              children: [
-                {
-                  text: 'Spring', 
-                  children:[
-                    { text: 'Spring Framework', link: '/framework/spring_framework/' },
-                    { text: 'Spring AOP', link: '/framework/spring_aop/' },
-                    { text: 'Spring Boot', link: '/framework/spring_boot/' },
-                    { text: 'Spring Cloud', link: '/framework/spring_cloud/' },
-                ]
-               }, 
-                {
-                  text: '网络通信', 
-                  children:[
-                    { text: '深入理解Netty', link: '/framework/netty/' }
-                ]
-               }
-             ]
-            },
-            {
-              text: '中间件', 
-              children:[
-                {text:'深入理解JVM', link: '/middleware/jvm/'},
-                {text:'RocketMQ从实践到原理', link: '/middleware/mq/'},
-                {text:'Tomcat核心原理', link: '/middleware/tomcat/'},
-            ]
+              }]
           },
-            {
-              text: '计算机原理',
-              children: [
-                {text:'操作系统', link: '/computer_theory/Os/'},
+          {
+              text: '常用框架',
+              children: [{
+                  text: 'Spring',
+                  children: [{
+                      text: 'Spring Framework',
+                      link: '/framework/spring_framework/'
+                  },
+                  {
+                      text: 'Spring AOP',
+                      link: '/framework/spring_aop/'
+                  },
+                  {
+                      text: 'Spring Boot',
+                      link: '/framework/spring_boot/'
+                  },
+                  {
+                      text: 'Spring Cloud',
+                      link: '/framework/spring_cloud/'
+                  },
+                  ]
+              },
+              {
+                  text: '网络通信',
+                  children: [{
+                      text: '深入理解Netty',
+                      link: '/framework/netty/'
+                  }]
+              }]
+          },
+          {
+              text: '中间件',
+              children: [{
+                  text: '深入理解JVM',
+                  link: '/middleware/jvm/'
+              },
+              {
+                  text: 'RocketMQ从实践到原理',
+                  link: '/middleware/mq/'
+              },
+              {
+                  text: 'Tomcat核心原理',
+                  link: '/middleware/tomcat/'
+              },
               ]
-            },
-            {text: '深度好文', link: '/favorite_article/'},
-          ],
+          },
+          {
+              text: '计算机原理',
+              children: [{
+                  text: '操作系统',
+                  link: '/computer_theory/Os/'
+              },
+              ]
+          },
+          {
+              text: '深度好文',
+              link: '/favorite_article/'
+          },
+          {
+            text: '关于',
+            link: '/about/'
+          },
+        ],
     }),
     extendsMarkdown: (md) => {
       md.use(MarkdownItKatex);
---
+++ docs/about/README.md
@@ -0,0 +1,8 @@
+---
+sidebar: false
+---
+# 更多的我
+
+社区：[掘金](https://juejin.cn/user/1345457965251991/posts)
+
+软考：[软考通](https://dtwr.gitee.io/software_exam/)
